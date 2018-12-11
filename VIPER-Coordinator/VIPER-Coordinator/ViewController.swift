90e1696e8ddca4e3015ed99698048577beece4c1
iBasel <basel.e@me.com>
2018-12-11 11:48:18 -0500

    initial commit

---
+++ VIPER-Coordinator/VIPER-Coordinator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPER-Coordinator
+//
+//  Created by Basel Abdelaziz on 12/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
